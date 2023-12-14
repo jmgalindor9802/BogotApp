70bd84985278400005c5b5c9b68d7e9ceba3c818
jmgalindor9802 <jmgalindor9802@gmail.com>
2023-12-13 22:25:41 -0500

    Primer avance

---
+++ app/src/main/java/com/example/bogotapp/MenuActivity.kt
@@ -0,0 +1,21 @@
+package com.example.bogotapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MenuActivity:AppCompatActivity{
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityMenuBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.recycler.adapter=SitiosAdapter(
+            listOf(
+                Sitio("Plaza de Bolivar","plazabolivar","La plaza de Bolívar es la plaza principal de la ciudad de Bogotá y de Colombia. Está ubicada en el centro histórico de la ciudad, entre las carreras Séptima y Octava con calles Décima y Once. A su alrededor se encuentran algunos de los principales edificios de la ciudad: al norte el Palacio de Justicia, al sur el Capitolio Nacional, al oriente la Catedral Basílica Metropolitana de Bogotá y Primada de Colombia, la Casa del Cabildo Eclesiástico, la Capilla del Sagrario de Catedral Basílica Metropolitana y el Palacio Arzobispal y al occidente el Palacio Liévano, sede de la Alcaldía Mayor de Bogotá, y al suroriente el Colegio Mayor de San Bartolomé (Bogotá) de los jesuitas. ",),
+                Sitio("Museo del oro","museo_oro","El Museo del Oro del Banco de la República de Colombia es una institución pública la cual su fin es la adquisición, conservación y exposición de piezas de orfebrería y alfarería de culturas indígenas del periodo precolombino de la actual Colombia, cuenta con más de 30 000 piezas hechas en oro, 20 000 de objetos líticos, cerámicos, piedras y textiles pertenecientes a las culturas Quimbaya, Calima, Tairona, Zenú, Muisca, Tolima, Tumaco entre otros."),
+                Sitio("Jardin Botánico","jardinb","El Jardín Botánico de Bogotá, nombrado oficialmente Jardín Botánico José Celestino Mutis en honor al astrónomo y botánico José Celestino Mutis, es un centro de investigación, conservación y divulgación de la diversidad de las especies vegetales en Bogotá, capital de Colombia. Fundado en 1955 por el sacerdote Enrique Pérez Arbeláez."),
+                Sitio("Museo Nacional","museo_nal","El Museo Nacional de Colombia, comúnmente abreviado como Museo Nacional o MNC, es el primer museo fundado en la República de Colombia. Cuenta con su sede en la capital, Bogotá. Fue fundado por ley del congreso constitucional de la Gran Colombia el 28 de julio de 1823, durante el primer mandato del gobierno encabezado por el presidente Simón Bolívar y el vicepresidente Francisco de Paula Santander. Sin embargo, inició su funcionamiento el 4 de julio del mismo año, cuando se declaró oficialmente creado. Esto lo convierte en la primera institución museal de Colombia y una de las más antiguas de América.")
+            )
+        )
+    }
+}
